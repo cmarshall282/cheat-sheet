566a820ed694c21c620b63136b233ba7a9132edf
Ben <bvazzana672@kirksville.k12.mo.us>
2018-05-09 22:14:20 -0500

    finished

--- src/org/marshallbros/cheatsheet/Main.java
+++ src/org/marshallbros/cheatsheet/Main.java
@@ -3,11 +3,19 @@ package org.marshallbros.cheatsheet;
 import java.awt.*;
 
 public class Main extends Canvas {
+
+    private static final int WIDTH = 640;
+    private static final int HEIGHT = WIDTH / 12 * 9;
+    private static final String title = "Cheat Sheet";
+
+    Thread thread;
+
     public Main() {
 
+        new Window(WIDTH, HEIGHT, title, this);
     }
 
-    public static void main() {
-
+    public static void main(String[] args) {
+        new Main();
     }
 }
