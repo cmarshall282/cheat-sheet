c742ed13e95230bbd2a2bd086a03ace29bc94e92
cmarshall282 <christopherscottmarshall@gmail.com>
2018-05-09 22:11:32 -0500

    First algebra functions created.

--- src/org/marshallbros/cheatsheet/Algebra.java
+++ src/org/marshallbros/cheatsheet/Algebra.java
@@ -1,7 +1,27 @@
 package org.marshallbros.cheatsheet;
 
+import java.awt.*;
+
 public class Algebra {
     public Algebra() {
 
+    }
+
+    public double[] quadraticFormula(double a, double b, double c) {
+        double[] answers = new double[2];
+
+        answers[0] = (-b + Math.sqrt(b * b - 4 * a * c)) / (2 * a);
+        answers[1] = (-b - Math.sqrt(b * b - 4 * a * c)) / (2 * a);
+
+        return answers;
+    }
+
+    public double[] findQuadraticVertex(double a, double b, double c) {
+        double[] answer = new double[2];
+
+        answer[0] = (-b) / (2 * a);
+        answer[1] = 0;
+
+        return answer;
     }
 }
