00ef2533fa5c99473ddb310267f87ef9e37e2ea1
cmarshall282 <christopherscottmarshall@gmail.com>
2018-05-19 14:35:34 -0500

    Updated solve triangle function

--- src/org/marshallbros/cheatsheet/Trigonometry.java
+++ src/org/marshallbros/cheatsheet/Trigonometry.java
@@ -14,33 +14,33 @@ public class Trigonometry {
 
         if(triangle.theta1 != -1.0) {
             if(triangle.a != -1.00) {
-                triangle.b = triangle.a / Math.tan(triangle.theta1);
-                triangle.c = triangle.a / Math.sin(triangle.theta1);
+                triangle.b = triangle.a / Math.tan(Math.toRadians(triangle.theta1));
+                triangle.c = triangle.a / Math.sin(Math.toRadians(triangle.theta1));
 
                 triangle.theta2 = 90 - triangle.theta1;
             } else if(triangle.b != -1.0) {
-                triangle.a = triangle.b * Math.tan(triangle.theta1);
-                triangle.c = triangle.b / Math.cos(triangle.theta1);
+                triangle.a = triangle.b * Math.tan(Math.toRadians(triangle.theta1));
+                triangle.c = triangle.b / Math.cos(Math.toRadians(triangle.theta1));
 
                 triangle.theta2 = 90 - triangle.theta1;
             } else if(triangle.c != -1.0) {
-                triangle.a = triangle.c * Math.sin(triangle.theta1);
-                triangle.b = triangle.c * Math.cos(triangle.theta1);
+                triangle.a = triangle.c * Math.sin(Math.toRadians(triangle.theta1));
+                triangle.b = triangle.c * Math.cos(Math.toRadians(triangle.theta1));
             }
         } else if(triangle.theta2 != -1.0) {
             if(triangle.a != -1.0) {
-                triangle.b = triangle.a * Math.tan(triangle.theta2);
-                triangle.c = triangle.a / Math.cos(triangle.theta2);
+                triangle.b = triangle.a * Math.tan(Math.toRadians(triangle.theta2));
+                triangle.c = triangle.a / Math.cos(Math.toRadians(triangle.theta2));
 
                 triangle.theta1 = 90 - triangle.theta2;
             } else if(triangle.b != -1.0) {
-                triangle.a = triangle.b / Math.tan(triangle.theta2);
-                triangle.c = triangle.b / Math.sin(triangle.theta2);
+                triangle.a = triangle.b / Math.tan(Math.toRadians(triangle.theta2));
+                triangle.c = triangle.b / Math.sin(Math.toRadians(triangle.theta2));
 
                 triangle.theta1 = 90 - triangle.theta2;
             } else if(triangle.c != -1.0) {
-                triangle.a = triangle.c * Math.cos(triangle.theta2);
-                triangle.b = triangle.c * Math.sin(triangle.theta2);
+                triangle.a = triangle.c * Math.cos(Math.toRadians(triangle.theta2));
+                triangle.b = triangle.c * Math.sin(Math.toRadians(triangle.theta2));
 
                 triangle.theta1 = 90 - triangle.theta2;
             }
@@ -48,17 +48,17 @@ public class Trigonometry {
             if(triangle.a != -1.0 && triangle.b != -1.0) {
                 triangle.c = Geometry.pythagoreanTheorem(triangle.a, triangle.b, -1.0);
 
-                triangle.theta1 = Math.atan2(triangle.a, triangle.b);
+                triangle.theta1 = Math.toDegrees(Math.atan2(triangle.a, triangle.b));
                 triangle.theta2 = 90 - triangle.theta1;
             } else if(triangle.a != -1.0 && triangle.c != -1.0) {
                 triangle.b = Geometry.pythagoreanTheorem(triangle.a, -1.0, triangle.c);
 
-                triangle.theta1 = Math.atan2(triangle.a, triangle.b);
+                triangle.theta1 = Math.toDegrees(Math.atan2(triangle.a, triangle.b));
                 triangle.theta2 = 90 - triangle.theta1;
             } else if(triangle.b != -1.0 && triangle.c != -1.0) {
                 triangle.a = Geometry.pythagoreanTheorem(-1.0, triangle.b, triangle.c);
 
-                triangle.theta1 = Math.atan2(triangle.a, triangle.b);
+                triangle.theta1 = Math.toDegrees(Math.atan2(triangle.a, triangle.b));
                 triangle.theta2 = 90 - triangle.theta1;
             }
         }
